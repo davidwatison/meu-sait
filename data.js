815dfdc34c3fe2aa69f2d1024fafb6b15d9fcd8e
davidwatison <00001134392126sp@al.educacao.sp.gov.br>
2025-06-09 14:54:34 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+// Dados que serão usados para busca
+var data = [
+    {
+        "id": "assunto1",
+        "title": "Assunto 1",
+        "content": "Conteúdo detalhado do Assunto 1. Aqui você coloca todas as informações relevantes sobre o Assunto 1."
+    },
+    {
+        "id": "assunto2",
+        "title": "Assunto 2",
+        "content": "Conteúdo detalhado do Assunto 2. Aqui você coloca todas as informações relevantes sobre o Assunto 2."
+    },
+    {
+        "id": "assunto3",
+        "title": "Assunto 3",
+        "content": "Conteúdo detalhado do Assunto 3. Aqui você coloca todas as informações relevantes sobre o Assunto 3."
+    }
+];
